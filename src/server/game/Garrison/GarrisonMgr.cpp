2d7f4f3d3e34ded715609e8349d40a95bc592c1b
Zerix <tkalmbach18@gmail.com>
2014-11-14 04:11:35 -0500

    Core/Garrisons: First steps in the garrison implementation.

--- src/server/game/Garrison/GarrisonMgr.cpp
+++ src/server/game/Garrison/GarrisonMgr.cpp
@@ -29,143 +29,116 @@
 #include "WorldPacket.h"
 #include "WorldSession.h"
 
-//GarrisonMgr::~GarrisonMgr()
-//{
-//    for (GarrisonSet::iterator itr = m_garrisonSet.begin(); itr != m_garrisonSet.end(); itr++)
-//        delete *itr;
-//
-//    m_garrisonSet.clear();
-//}
-//
-//void GarrisonMgr::LoadFromDB(PreparedQueryResult result)
-//{
-//    if (!result)
-//        return;
-//    
-//    do
-//    {
-//        Field* fields = result->Fetch();
-//
-//        uint64 garrisonId = fields[0].&Field::GetUInt64;
-//        uint32 accountId = fields[1].GetUInt32;
-//        uint32 characterId = fields[2].GetUInt32;
-//        uint32 garrisonLevel = fields[3].GetUInt32;
-//        uint32 currentResources = fields[4].GetUInt32;
-//        uint8  specialization = fields[5].GetUInt8;
-//        uint32 buildings = fields[6].GetUInt32;
-//        uint32 workOrders = fields[7].GetUInt32;
-//
-//        Garrisons* garrison = new Garrisons(garrisonId, accountId, characterId, garrisonLevel, currentResources,
-//            specialization, buildings, workOrders);
-//
-//        m_garrisonSet.insert(garrison);
-//
-//    } while (result->NextRow());
-//}
-//
-//void GarrisonMgr::SaveToDB(SQLTransaction& trans)
-//{
-//    SaveToDB(trans);
-//
-//    if (m_garrisonSet.empty())
-//        return;
-//
-//    GarrisonSet::iterator itr = m_garrisonSet.begin();
-//    while (itr != m_garrisonSet.end())
-//    {
-//        Garrisons* garrison = *itr++;
-//        switch (garrison->GetDbState())
-//        {
-//        case DB_STATE_GARR_NONE:
-//            break;
-//        case DB_STATE_GARR_DELETE:
-//        {
-//            PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_GARRISON);
-//            stmt->setUInt64(0, garrison->GetGarrisonId());
-//            trans->Append(stmt);
-//
-//            m_garrisonSet.erase(itr);
-//            delete garrison;
-//
-//            break;
-//        }
-//        case DB_STATE_GARR_SAVE:
-//        {
-//            PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_GARRISON);
-//            stmt->setUInt64(0, garrison->GetGarrisonId());
-//            trans->Append(stmt);
-//
-//            stmt = CharacterDatabase.GetPreparedStatement(CHAR_INS_GARRISON);
-//            stmt->setUInt64(0, garrison->GetGarrisonId());
-//            stmt->setUInt32(1, garrison->GetAccountId());
-//            stmt->setUInt32(2, garrison->GetCharacterId());
-//            stmt->setUInt32(3, garrison->GetGarrisonLevel());
-//            stmt->setUInt32(4, garrison->GetGarrisonResources());
-//            stmt->setUInt8(5, garrison->GetGarrisonSpec());
-//            stmt->setUInt32(6, garrison->GetGarrisonBuildings());
-//            stmt->setUInt32(7, garrison->GetGarrisonWorkOrders());
-//            trans->Append(stmt);
-//
-//            garrison->SetDbState(DB_STATE_GARR_NONE);
-//            break;
-//        }
-//        default:
-//            break;
-//        }
-//    }
-//}
-//
-//void GarrisonMgr::LoadFollowerFromDB(PreparedQueryResult result)
-//{
-//    if (!result)
-//        return;
-//
-//    do
-//    {
-//        Field* field = result->Fetch();
-//
-//        uint64 garrisonId = field[0].GetUInt64();
-//        uint32 followerSlot1 = field[1].GetUInt32();
-//        uint32 followerSlot2 = field[2].GetUInt32();
-//        uint32 followerSlot3 = field[3].GetUInt32();
-//        uint32 followerSlot4 = field[4].GetUInt32();
-//        uint32 followerSlot5 = field[5].GetUInt32();
-//        uint32 slot1Level    = field[6].GetUInt32();
-//        uint32 slot2Level = field[7].GetUInt32();
-//        uint32 slot3Level = field[8].GetUInt32();
-//        uint32 slot4Level = field[9].GetUInt32();
-//        uint32 slot5Level = field[10].GetUInt32();
-//        uint32 slot1XpToLevel = field[11].GetUInt32();
-//        uint32 slot2XpToLevel = field[12].GetUInt32();
-//        uint32 slot3XpToLevel = field[13].GetUInt32();
-//        uint32 slot4XpToLevel = field[14].GetUInt32();
-//        uint32 slot5XpToLevel = field[15].GetUInt32();
-//
-//        Followers* follower = new Followers(garrisonId, followerSlot1, followerSlot2, followerSlot3, followerSlot4, followerSlot5,
-//            slot1Level, slot2Level, slot3Level, slot4Level, slot5Level, slot1XpToLevel, slot2XpToLevel, slot3XpToLevel, slot4XpToLevel, slot5XpToLevel);
-//    } while (result->NextRow());
-//}
-//
-//Garrisons* GarrisonMgr::GetGarrison(uint64 garrisonId) const
-//{
-//    for (GarrisonSet::iterator itr = m_garrisonSet.begin(); itr != m_garrisonSet.end(); itr++)
-//        if ((*itr)->GetGarrisonId() == garrisonId)
-//            return *itr;
-//
-//    return NULL;
-//}
-//
-//uint8 GarrisonMgr::GarrisonGetFollowerSlots(uint64 garrisonId)
-//{
-//    for (uint8 i = 0; i < MAX_FOLLOWER_SLOTS; i++)
-//        if (GetFollowerSlot(i) == garrisonId)
-//            return i;
-//
-//    return 0;
-//}
-//
-
-void GarrisonMgr::SendGarrisonGetInfo()
+GarrisonMgr::~GarrisonMgr()
 {
-    HandleGetGarrisonInfoResult();
+	for (auto itr : m_GarrisonsStore)
+		delete itr;
+
+	for (auto itr : m_GarrisonInfoStore)
+		delete itr;
+
+	for (auto itr : m_FollowerStore)
+		delete itr;
+
+	for (auto itr : m_BuildingSet)
+		delete itr;
+
+	for (auto itr : m_PlotInfoStore)
+		delete itr;
+
+	m_GarrisonsStore.clear();
+	m_GarrisonInfoStore.clear();
+	m_FollowerStore.clear();
+	m_BuildingSet.clear();
+	m_PlotInfoStore.clear();
+}
+
+/* I WANT TO REWORK THE GARRISON SYSTEM ENTIRELY, NOW THAT I'VE PLAYED IT FOR REAL, I KNOW HOW IT SHOULD BE DONE*/
+
+void GarrisonMgr::SendGarrisonGetInfo(WorldSession* session)
+{
+	TC_LOG_DEBUG("network", "World: Sent SMSG_GET_GARR_INFO_RESULT");
+
+	WorldPacket data(SMSG_GET_GARR_INFO_RESULT, 500);				// SizeUnknown
+
+	for (GarrisonInfoSet::const_iterator itr = m_GarrisonInfoStore.begin(); itr != m_GarrisonInfoStore.end(); itr++)
+	{
+		const GarrisonInfo* info = *itr;
+
+		data << uint32(info->GarrisonBuildingInfoCount); // 21
+		data << uint32(0); // 22
+		data << uint32(0); // 8
+
+		data << uint32(info->GarrisonBuildingInfoCount);
+		data << uint32(info->GarrisonPlotInfoCount);
+		data << uint32(info->GarrisonFollowerCount);
+		data << uint32(info->GarrisonMission);
+		data << uint32(info->ArchivedMissions);
+
+
+		if (info->GarrisonBuildingInfoCount)
+		{
+			for (auto itr : m_BuildingSet)
+			{
+				const GarrisonBuildingInfo* building = itr;
+
+				data << uint32(building->GarrPlotInstanceID);
+				data << uint32(building->GarrBuildingID);
+				data << uint32(building->Time);
+				data << uint32(0);
+				data << uint32(building->CurrentGarSpecID);
+				data.WriteBit(building->Active);
+			}
+		}
+
+		if (info->GarrisonPlotInfoCount)
+		{
+			for (auto itr : m_PlotInfoStore)
+			{
+				const PlotInfo* plot = itr;
+
+				data << uint32(plot->GarrPlotInstanceID);
+				data << float(plot->PosX);
+				data << float(plot->PosY);
+				data << float(plot->PosZ);
+				data << uint32(plot->PlotType);
+			}
+		}
+
+		for (uint32 i = 0; i < info->GarrisonFollowerCount; i++)
+		{
+			// sub_61D8C6
+			data << uint64(0); // DbID
+			data << uint32(0); // 2
+			data << uint32(0); // 3
+			data << uint32(0); // 4
+			data << uint32(0); // 5
+			data << uint32(0); // 6
+			data << uint32(0); // 7
+			data << uint32(0); // 8
+			data << uint32(0); // 9
+			data << uint32(info->AbilityIDCount);
+			data << uint32(0); // 14
+			for (uint32 z = 0; z < info->AbilityIDCount; z++)
+				data << uint32(0); // AbilityID
+		}
+
+		for (uint32 i = 0; i < info->GarrisonMission; i++)
+		{
+			// sub_61BED1
+			data << uint64(0); // DbID
+			data << uint32(0);
+			data << uint32(0);
+			data << uint32(0);
+			data << uint32(0);
+			data << uint32(0);
+			data << uint32(0);
+			data << uint32(0);
+		}
+
+		for (uint32 i = 0; i < info->ArchivedMissions; i++)
+			data << uint32(info->ArchivedMissions);
+	}
+
+	session->SendPacket(&data);
 }
--- src/server/game/Garrison/GarrisonMgr.h
+++ src/server/game/Garrison/GarrisonMgr.h
@@ -28,207 +28,105 @@
 #include "DB2Stores.h"
 #include "DB2Enums.h"
 
-#define MAX_BUILDING_PLOTS 3
-#define MAX_FOLLOWER_SLOTS 5
 #define MAX_GARRISON_LEVEL 3
 #define MAX_RESOURCES      10000
 
-enum DBStateGarr
+#pragma once
+struct Garrisons
 {
-	DB_STATE_GARR_NONE = 0,
-	DB_STATE_GARR_SAVE = 1,
-	DB_STATE_GARR_DELETE = 2
+	Garrisons();
+public:
+
 };
 
-enum DBStateFollowers
+struct Followers
 {
-	DB_STATE_FOLLOWER_NONE = 0,
-	DB_STATE_FOLLOWER_SAVE = 1,
-	DB_STATE_FOLLOWER_DELETE = 2
+	Followers();
+public:
+
 };
+
+struct GarrisonInfo
+{
+	GarrisonInfo(uint32 infoCount, uint32 plotInfoCount, uint32 followerCount, uint32 mission, uint32 archivedMissions, uint32 abilityCount)
+		: GarrisonBuildingInfoCount(infoCount), GarrisonPlotInfoCount(plotInfoCount), GarrisonFollowerCount(followerCount), GarrisonMission(mission),
+		ArchivedMissions(archivedMissions), AbilityIDCount(abilityCount) { }
+		
+public:
+	uint32 GarrisonBuildingInfoCount;
+	uint32 GarrisonPlotInfoCount;
+	uint32 GarrisonFollowerCount;
+	uint32 GarrisonMission;
+	uint32 ArchivedMissions;
+	uint32 AbilityIDCount;
+};
+
+struct GarrisonBuildingInfo
+{
+	GarrisonBuildingInfo(uint32 plotId, uint32 buildingId, uint32 time, uint32 specId, bool active)
+		: GarrPlotInstanceID(plotId), GarrBuildingID(buildingId), Time(time), CurrentGarSpecID(specId), Active(active) { }
+
+public:
+	uint32 GarrPlotInstanceID;
+	uint32 GarrBuildingID;
+	uint32 Time;
+	uint32 CurrentGarSpecID;
+	bool Active;
+};
+
+struct PlotInfo 
+{
+	PlotInfo(uint32 Id, float X, uint32 Y, uint32 Z, uint32 plotType)
+	: GarrPlotInstanceID(Id), PosX(X), PosY(Y), PosZ(Z), PlotType(plotType) { }
+
+public:
+	uint32 GarrPlotInstanceID;
+	float PosX;
+	float PosY;
+	float PosZ;
+	uint32 PlotType;
+};
+
+typedef std::set <Garrisons*> GarrisonsSet;
+typedef std::set <Followers*> FollowerSet;
+typedef std::set <GarrisonInfo*> GarrisonInfoSet;
+typedef std::set <GarrisonBuildingInfo*> BuildingSet;
+typedef std::set <PlotInfo*> PlotInfoSet;
 
 class GarrisonMgr
 {
 public:
-    GarrisonMgr();
-    ~GarrisonMgr();
 
-    void HandleGetGarrisonInfoResult();
+	static GarrisonMgr* instance()
+	{
+		static GarrisonMgr instance;
+		return &instance;
+	}
 
-    uint32 GetGarrisonId() const { return m_garrId; }
-    uint32 GetAccountId() const { return m_accountId; }
-    uint32 GetCharacterId() const { return m_charId; }
-    uint32 GetGarrisonLevel() const { return m_garrLevel; }
-    uint32 GetGarrisonResources() const { return m_currentResources; }
-    uint8 GetGarrisonSpec() const { return m_specialization; }
-    uint32 GetGarrisonBuildings() const { return m_buildings; }
-    uint32 GetGarrisonWorkOrders() const { return m_workOrders; }
+	GarrisonMgr();
+	~GarrisonMgr();
 
-    Player* GetOwner() const { return m_owner; }
-    //Garrisons* GetGarrison(uint64 garrisonId) const;
-    void GarrisonCreate();
+	void LoadFromDB(PreparedQueryResult result) { };
 
-    void Reset();
-    static void DeleteFromDB(uint32 lowguid);
-    void LoadFromDB(PreparedQueryResult result) {};
-    void SaveToDB(SQLTransaction& trans);
+	void SendGarrisonGetInfo(WorldSession* session);
+	void HandleGetGarrisonInfoResult();
 
-    static void DeleteFollowerFromDB(uint32 garrId);
-    void SaveFollowerToDB(SQLTransaction& trans);
-    void LoadFollowerFromDB(PreparedQueryResult result) {};
-
-    CreatureTemplate* GetCreatureTemplate() const { return m_creature; }
-
-    // Garrison Functions
-    GarrMissionCategory* GetMissionCategory() const { return m_category; }
-    GarrMissionEntry* GetMission() const { return m_garrMission; }
-    GarrAbilityEntry* GetAbility() const { return m_garrAbility; }
-
-    void SendGarrisonGetInfo();
-
-    uint8 GarrisonGetFollowerSlots(uint64 garrisonId);
-    uint8 GetFollowerSlot(uint32 followerId);
+	void GarrisonCreate();
 
 private:
-    Player* m_owner;
-    Unit* m_unit;
+	GarrisonsSet m_GarrisonsStore;
+	FollowerSet m_FollowerStore;
+	GarrisonInfoSet m_GarrisonInfoStore;
+	BuildingSet m_BuildingSet;
+	PlotInfoSet m_PlotInfoStore;
 
-    uint32 m_garrId;
-    uint32 m_accountId;
-    uint32 m_charId;
-    uint32 m_garrLevel;
-    uint32 m_currentResources;
-    uint8 m_specialization;
-    uint32 m_buildings;
-    uint32 m_workOrders;
-
-    CreatureTemplate* m_creature;
-    GarrAbilityEntry* m_garrAbility;
-    GarrMissionEntry* m_garrMission;
-    GarrMissionCategory* m_category;
-
-    //GarrisonSet m_garrisonSet;
+	void LoadFollowersFromDb();
+	void LoadBuildingsFromDb();
+	void LoadMissionsFromDb();
+	void LoadWorkOrdersFromDb();
+	void LoadAbilitiesFromDb();
 };
 
-//typedef std::set<Garrisons*> GarrisonSet;
-
-#pragma once
-class Garrisons
-{
-public:
-	Garrisons(uint64 garrisonId, uint32 accountId, uint32 characterId, uint32 garrisonLevel, uint32 currentResources, uint8 specialization,
-		uint32 garrisonBuildings, uint32 garrisonWorkOrders) : m_garrId(garrisonId), m_accountId(accountId), m_charId(characterId), m_garrLevel(garrisonLevel),
-		m_currentResources(currentResources), m_specialization(specialization), m_buildings(garrisonBuildings), m_workOrders(garrisonWorkOrders) { }
-	~Garrisons();
-
-	Player* GetOwner() const { return m_owner; }
-
-	uint64 GetGarrisonId() const { return m_garrId; }
-	uint32 GetAccountId() const { return m_accountId; }
-	uint32 GetCharacterId() const { return m_charId; }
-	uint32 GetGarrisonLevel() const { return m_garrLevel; }
-	uint32 GetGarrisonResources() const { return m_currentResources; }
-	uint8 GetGarrisonSpec() const { return m_specialization; }
-	uint32 GetGarrisonBuildings() const { return m_buildings; }
-	uint32 GetGarrisonWorkOrders() const { return m_workOrders; }
-
-	DBStateGarr GetDbState() const { return m_dbState; }
-	void SetDbState(DBStateGarr state) { m_dbState = state; }
-
-	// Garrison Functions
-	void SendArchitectWindow(ObjectGuid npcGuid);
-	void SendGarrisonUpgrade();
-	void SendGarrisonLevelUp();
-
-	uint32 GetGarrisonPlot() const { return m_plot; }
-
-	void CreateGarrison();
-	void DeleteGarrison();
-
-	// Building Functions
-	void SendGarrisonBuildingActivate();
-	void GarrisonPlaceBuilding();
-	void GarrisonDeleteBuilding();
-	void GarrisonSelectBuildingSpecialization();
-	void GarrisonSwapBuilding();
-
-	void SendGarrisonPlotPlaced();
-	void SendGarrisonPlotCleared();
-
-	// Follower Functions
-	void SendGarrisonAddFollower();
-	void SendGarrisonRemoveFollower();
-	void HandleGarrisonFollowerLevel();
-	void HandleGarrisonFollowerItemLevelChange();
-	uint32 GetFollowerAbility() const { return m_followerAbility; }
-	uint32 GetFollowerId() const { return m_garrFollower; }
-
-	// Mission functions
-	void SendGarrCompleteMission();
-	void SendGarrStartMission();
-	void GarrisonSelectFollowersForMission();
-	void SendGarrOpenMissionNpc();
-	void SendGarrAddMission();
-
-	void HandleGarrisonMissionType(PreparedQueryResult result);
-
-private:
-	Player* m_owner;
-
-	// Struct of garrison table
-	uint64 m_garrId;
-	uint32 m_accountId;
-	uint32 m_charId;
-	uint32 m_garrLevel;
-	uint32 m_currentResources;
-	uint8 m_specialization;
-	uint32 m_buildings;
-	uint32 m_workOrders;
-
-	uint32 m_followerAbility;
-	uint32 m_plot;
-
-	CreatureTemplate* m_creature;
-	GarrAbilityEntry* m_garrAbility;
-	GarrMissionEntry* m_garrMission;
-	GarrMissionCategory* m_category;
-	uint32 m_garrFollower;
-
-	DBStateGarr m_dbState;
-};
-
-class Followers
-{
-public:
-	Followers(uint32 garrisonId, uint32 slot1, uint32 slot2, uint32 slot3, uint32 slot4, uint32 slot5, uint32 slotLevel1, uint32 slotLevel2, uint32 slotLevel3,
-		uint32 slotLevel4, uint32 slotLevel5, uint32 slot1XpToLevel, uint32 slot2XpToLevel, uint32 slot3XpToLevel, uint32 slot4XpToLevel, uint32 slot5XpToLevel)
-		: m_garrisonId(garrisonId), m_slot1(slot1), m_slot2(slot2), m_slot3(slot3), m_slot4(slot4), m_slot5(slot5), m_slotLevel1(slotLevel1), m_slotLevel2(slotLevel2),
-		m_slotLevel3(slotLevel3), m_slotLevel4(slotLevel4), m_slotLevel5(slotLevel5), m_slot1XpToLevel(slot1XpToLevel), m_slot2XpToLevel(slot2XpToLevel),
-		m_slot3XpToLevel(slot3XpToLevel), m_slot4XpToLevel(slot4XpToLevel), m_slot5XpToLevel(slot5XpToLevel) { }
-	~Followers();
-
-	DBStateFollowers GetDbState() const { return m_dbState; }
-	void SetDbState(DBStateFollowers state) { m_dbState = state; }
-
-private:
-	uint32 m_garrisonId;
-	uint32 m_slot1;
-	uint32 m_slot2;
-	uint32 m_slot3;
-	uint32 m_slot4;
-	uint32 m_slot5;
-	uint32 m_slotLevel1;
-	uint32 m_slotLevel2;
-	uint32 m_slotLevel3;
-	uint32 m_slotLevel4;
-	uint32 m_slotLevel5;
-	uint32 m_slot1XpToLevel;
-	uint32 m_slot2XpToLevel;
-	uint32 m_slot3XpToLevel;
-	uint32 m_slot4XpToLevel;
-	uint32 m_slot5XpToLevel;
-
-	DBStateFollowers m_dbState;
-};
+#define sGarrisonMgr GarrisonMgr::instance()
 
 #endif

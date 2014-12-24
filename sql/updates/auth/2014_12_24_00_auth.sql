9990c1b1c9872a9f3a2722ea0f6d5c0a0b34ed1e
SkyFire <admin@projectskyfire.org>
2014-12-24 07:16:58 -0600

    Some cleanup in the auth DB Don't know how or why theses tables were in there.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

---
+++ sql/updates/auth/2014_12_24_00_auth.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `creature`;
+DROP TABLE IF EXISTS `creature_equip_template`;
+DROP TABLE IF EXISTS `creature_summon_groups`;
+DROP TABLE IF EXISTS `dbc_faction`;
